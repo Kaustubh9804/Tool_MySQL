97b7ab557d4ba8db007d9b6803fbf59c74c0b7d1
Kaustubh9804 <164786212+Kaustubh9804@users.noreply.github.com>
2024-09-25 13:59:36 +0530

    Add files via upload

---
+++ Terraform/private_RT/variable.tf
@@ -0,0 +1,19 @@
+variable "vpc_id" {
+  type = string
+}
+
+variable "nat_gateway_id" {
+  type = string
+}
+
+variable "Private_subnet1_id" {
+  type = string
+}
+
+variable "Private_subnet2_id" {
+  type = string
+}
+
+variable "private_route_table_name" {
+  type = string
+}
