ff00520950bb06d414a189fadc698dd60f313865
Kaustubh9804 <164786212+Kaustubh9804@users.noreply.github.com>
2024-09-25 13:57:42 +0530

    Add files via upload

---
+++ Terraform/public_RT/variable.tf
@@ -0,0 +1,15 @@
+variable "vpc_id" {
+  type = string
+}
+
+variable "internet_gateway_id" {
+  type = string
+}
+
+variable "Public_subnet_id" {
+  type = string
+}
+
+variable "public_route_table_name" {
+  type = string
+}
