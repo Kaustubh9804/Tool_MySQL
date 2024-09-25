38acefe7b066b831632de511ca3febfa452dbe4e
Kaustubh9804 <164786212+Kaustubh9804@users.noreply.github.com>
2024-09-25 14:07:46 +0530

    Create main.tf

---
+++ Terraform/security_group/main.tf
@@ -0,0 +1,43 @@
+resource "aws_security_group" "Public_SG" {
+  vpc_id = var.vpc_id
+  dynamic "ingress" {
+    for_each = var.Public_ingress_port
+    content {
+    from_port = ingress.value
+    to_port   = ingress.value
+    protocol  = "tcp"
+    cidr_blocks = var.Public_ingress_cidr_block
+    }
+  }
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = var.Public_SG_name
+  }
+}
+
+resource "aws_security_group" "Private_SG" {
+  vpc_id = var.vpc_id
+  dynamic "ingress" {
+    for_each = var.Private_ingress_port
+    content {
+    from_port = ingress.value
+    to_port   = ingress.value
+    protocol  = "tcp"
+    cidr_blocks = var.Public_ingress_cidr_block
+    }
+  }
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = var.Public_SG_name
+  }
+}
