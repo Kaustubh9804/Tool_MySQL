a62bf9aad59708e070fda3d8aa71f5a1cf857027
Kaustubh9804 <164786212+Kaustubh9804@users.noreply.github.com>
2024-10-01 01:51:54 +0530

    Update main.tf

--- Terraform/public_RT/main.tf
+++ Terraform/public_RT/main.tf
@@ -15,3 +15,9 @@ resource "aws_route_table_association" "public_association" {
   subnet_id = var.Public_subnet_id
   route_table_id = aws_route_table.Public_RT.id
 }
+
+resource "aws_route" "RT-1" {
+  route_table_id = aws_route_table.Public_RT.id
+  destination_cidr_block = var.default_vpc_cidr
+  vpc_peering_connection_id = var.vpc_peering_id
+}
